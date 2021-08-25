508890309c71a4e73993463878dcec2b25ab3014
gness1804 <graham.nessler@gmail.com>
2021-08-24 19:30:17 -0500

    👌 IMPROVE: show the movie titles using subscribe

--- chapter05/start_here/rxjs-operators-instance-methods/src/app/app.component.ts
+++ chapter05/start_here/rxjs-operators-instance-methods/src/app/app.component.ts
@@ -1,30 +1,31 @@
 import { Component } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   subscription: Subscription = null;
   inputStreamData = ['john wick', 'inception', 'interstellar'];
   outputStreamData = [];
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   startStream() {
     const streamSource = interval(1500);
-    this.subscription = streamSource.subscribe(input => {
-      this.outputStreamData.push(input);
-    });
+    this.subscription = streamSource
+      .pipe(map((output) => output % this.inputStreamData.length))
+      .pipe(map((index) => this.inputStreamData[index]))
+      .subscribe((element) => {
+        this.outputStreamData.push(element);
+      });
   }
 
   stopStream() {
     this.subscription.unsubscribe();
     this.subscription = null;
   }
-
-
 }
