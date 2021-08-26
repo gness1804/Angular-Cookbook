9bd17efd737b6331c85982fb5df348c0fb5e9c18
gness1804 <graham.nessler@gmail.com>
2021-08-25 20:11:37 -0500

    📦 NEW: add logic to subscribe and unsubscribe from three streams

--- chapter05/start_here/rxjs-unsubscribing-streams/src/app/home/home.component.ts
+++ chapter05/start_here/rxjs-unsubscribing-streams/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { takeWhile } from 'rxjs/operators';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { interval } from 'rxjs/internal/observable/interval';
 import { Subscription } from 'rxjs/internal/Subscription';
@@ -8,28 +9,56 @@ import { Subscription } from 'rxjs/internal/Subscription';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  subscription: Subscription = null;
+  firstSubscription: Subscription = null;
+  secondSubscription: Subscription = null;
+  thirdSubscription: Subscription = null;
   inputStreamData = ['john wick', 'inception', 'interstellar'];
   outputStreamData = [];
+  isComponentAlive: boolean = false;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isComponentAlive = true;
+  }
 
   ngOnDestroy() {
-    this.stopStream();
+    this.isComponentAlive = false;
   }
 
   startStream() {
-    const streamSource = interval(1500);
-    this.subscription = streamSource.subscribe((input) => {
-      this.outputStreamData.push(input);
-      console.log('stream output', input);
-    });
+    this.isComponentAlive = true;
+    const firstStreamSource = interval(1500);
+    this.firstSubscription = firstStreamSource
+      .pipe(takeWhile(() => !!this.isComponentAlive))
+      .subscribe((input) => {
+        this.outputStreamData.push(input);
+        console.log('stream output', input);
+      });
+    const secondStreamSource = interval(3000);
+    this.secondSubscription = secondStreamSource
+      .pipe(takeWhile(() => !!this.isComponentAlive))
+      .subscribe((input) => {
+        this.outputStreamData.push(input);
+        console.log('stream output', input);
+      });
+    const thirdStreamSource = interval(500);
+    this.thirdSubscription = thirdStreamSource
+      .pipe(takeWhile(() => !!this.isComponentAlive))
+      .subscribe((input) => {
+        this.outputStreamData.push(input);
+        console.log('stream output', input);
+      });
   }
 
   stopStream() {
-    this.subscription.unsubscribe();
-    this.subscription = null;
+    this.isComponentAlive = false;
+    this.firstSubscription.unsubscribe();
+    this.secondSubscription.unsubscribe();
+    this.thirdSubscription.unsubscribe();
+
+    this.firstSubscription = null;
+    this.secondSubscription = null;
+    this.thirdSubscription = null;
   }
 }
